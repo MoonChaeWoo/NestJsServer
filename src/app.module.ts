576fcadc88b2c205148c69ae33f78bf0a4c335a0
tyche0529 <tyche0529@gmail.com>
2023-11-05 06:11:34 +0900

    db 모델 생성 학습

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModel } from './posts/entities/post.entities';
 
 // 의존성을 관리하는 곳
 @Module({
@@ -14,7 +15,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
               username: 'postgres',
               password: '1q2w3e4r!',
               database: 'NestJs_DataBase',
-              entities: [], // 생성할 모델들이 들어가는 곳
+              entities: [
+                PostModel, // entities의 class를 주입
+              ], // 생성할 모델들이 들어가는 곳
               synchronize: true, // 개발 시 true, 배포 시 false
             })],
   controllers: [AppController],
